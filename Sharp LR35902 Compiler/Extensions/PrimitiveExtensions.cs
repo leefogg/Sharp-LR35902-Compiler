ac1a9940b8ddd3d02def3182147f4ab4fb42b883
Lee Fogg <leefogg@users.noreply.github.com>
2018-07-26 11:49:38 +0100

    Minor readability change to GetHexBytes extension method

--- Sharp LR35902 Compiler/Extensions/PrimitiveExtensions.cs
+++ Sharp LR35902 Compiler/Extensions/PrimitiveExtensions.cs
@@ -25,10 +25,10 @@ namespace Sharp_LR35902_Compiler.Extensions
 			if (hex.Length % 2 == 1)
 				throw new ArgumentException("The hex code cannot have an odd number of digits");
 
-			byte[] arr = new byte[hex.Length >> 1];
+			byte[] arr = new byte[hex.Length / 2];
 
-			for (int i = 0; i < hex.Length >> 1; ++i)
-				arr[i] = (byte)((GetHexVal(hex[i << 1]) << 4) + (GetHexVal(hex[(i << 1) + 1])));
+			for (int i = 0; i < hex.Length / 2; ++i)
+				arr[i] = (byte)((GetHexVal(hex[i * 2]) << 4) + (GetHexVal(hex[(i * 2) + 1])));
 
 			return arr;
 		}
