97cd2ea8270198328779d86a88bb19c5c70ffb19
Lee Fogg <leefogg@users.noreply.github.com>
2018-08-08 12:08:51 +0100

    Added basic formatter

---
+++ Assembler/Formatter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp_LR35902_Assembler
+{
+    public class Formatter
+    {
+		public static void Format(List<string> instructions)
+		{
+			for (var i = 0; i < instructions.Count; i++)
+			{
+				//TODO: Convert this all to use StringBuilder to create much less garbage
+
+				var line = instructions[i].Trim();
+				while (line.Contains("  "))
+					line = line.Replace("  ", " ");
+
+				var lastspace = line.LastIndexOf(' ');
+				if (lastspace > line.IndexOf(' '))
+					if (line[lastspace-1] != ',')
+					line = line.Substring(0, lastspace) + "," + line.Substring(lastspace, line.Length - lastspace);
+
+				instructions[i] = line;
+			}
+		}
+    }
+}
---
+++ Sharp LR35902 Assembler Tests/Formatter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static Sharp_LR35902_Assembler.Formatter;
+using static Sharp_LR35902_Compiler_Tests.Utils;
+
+namespace Sharp_LR35902_Assembler_Tests
+{
+	[TestClass]
+    public class Formatter
+    {
+		[TestMethod]
+		public void Format_TrimsWhitespace()
+		{
+			var instructions = new List<string>()
+			{
+				"	 EI		"
+			};
+
+			Format(instructions);
+
+			listEqual(new[] { "EI" }, instructions.ToArray());
+		}
+
+		[TestMethod]
+		public void Format_SingleSpacesOnly()
+		{
+			var instructions = new List<string>()
+			{
+				"LD   A,   01"
+			};
+
+			Format(instructions);
+
+			listEqual(new[] { "LD A, 01" }, instructions.ToArray());
+		}
+
+		[TestMethod]
+		public void Format_AddsComma()
+		{
+			var instructions = new List<string>()
+			{
+				"LD A 01"
+			};
+
+			Format(instructions);
+
+			listEqual(new[] { "LD A, 01" }, instructions.ToArray());
+		}
+	}
+}
