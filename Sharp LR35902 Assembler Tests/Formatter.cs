121dedba8236158b25ff98d3aec685ebaf2f686d
Lee Fogg <leefogg@users.noreply.github.com>
2018-08-16 11:36:01 +0100

    Remove comments and remove blank lines

--- Sharp LR35902 Assembler Tests/Formatter.cs
+++ Sharp LR35902 Assembler Tests/Formatter.cs
@@ -16,7 +16,7 @@ namespace Sharp_LR35902_Assembler_Tests
 				"	 EI		"
 			};
 
-			RemoveWhitespaceAndAddComma(instructions);
+			RemoveWhitespace(instructions);
 
 			listEqual(new[] { "EI" }, instructions.ToArray());
 		}
@@ -29,7 +29,7 @@ namespace Sharp_LR35902_Assembler_Tests
 				"LD   A,   01"
 			};
 
-			RemoveWhitespaceAndAddComma(instructions);
+			RemoveWhitespace(instructions);
 
 			listEqual(new[] { "LD A, 01" }, instructions.ToArray());
 		}
@@ -42,7 +42,7 @@ namespace Sharp_LR35902_Assembler_Tests
 				"LD A 01"
 			};
 
-			RemoveWhitespaceAndAddComma(instructions);
+			AddComma(instructions);
 
 			listEqual(new[] { "LD A, 01" }, instructions.ToArray());
 		}
@@ -89,6 +89,50 @@ namespace Sharp_LR35902_Assembler_Tests
 					"	XOR A",
 					"label2:",
 					"XOR A"
+				},
+				instructions.ToArray()
+			);
+		}
+
+		[TestMethod]
+		public void RemoveBlankLines_Removes()
+		{
+			var instructions = new List<string>()
+			{
+				"",
+				"XOR A",
+				"",
+			};
+
+			RemoveBlankLines(instructions);
+
+			listEqual(
+				new[]
+				{
+					"XOR A"
+				},
+				instructions.ToArray()
+			);
+		}
+
+		[TestMethod]
+		public void RemoveComments_Removes()
+		{
+			var instructions = new List<string>()
+			{
+				"; This is a comment",
+				"EI; Same line comment",
+				"",
+			};
+
+			RemoveComments(instructions);
+
+			listEqual(
+				new[]
+				{
+					"",
+					"EI",
+					""
 				},
 				instructions.ToArray()
 			);
--- Sharp LR35902 Assembler/Formatter.cs
+++ Sharp LR35902 Assembler/Formatter.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Common.Extensions;
 
 namespace Sharp_LR35902_Assembler
 {
 	public class Formatter
     {
+		private static readonly Regex WhitespaceRegex = new Regex(@"\s+", RegexOptions.Compiled);
+
 		public static void Format(List<string> instructions)
 		{
+			RemoveComments(instructions);
 			LineBreakLabels(instructions);
-			RemoveWhitespaceAndAddComma(instructions);
+			RemoveWhitespace(instructions);
+			RemoveBlankLines(instructions);
+			AddComma(instructions);
+		}
+
+
+		public static void RemoveComments(List<string> instructions)
+		{
+			for (var i = 0; i < instructions.Count; i++)
+			{
+				var instruction = instructions[i];
+				var indexofcomment = instruction.IndexOf(';');
+				if (indexofcomment == -1)
+					return;
+
+				instructions[i] = instruction.Substring(0, indexofcomment);
+			}
+		}
+
+		public static void RemoveBlankLines(List<string> instructions)
+		{
+			for (var i = 0; i < instructions.Count; i++)
+			{
+				if (instructions[i].Length == 0)
+				{
+					instructions.RemoveAt(i);
+					i--;
+				}
+			}
+		}
+
+		public static void RemoveWhitespace(List<string> instructions)
+		{
+			for (var i = 0; i < instructions.Count; i++)
+			{
+				var line = instructions[i];
+				line = WhitespaceRegex.Replace(line, " ").Trim();
+				instructions[i] = line;
+			}
 		}
 
 		public static void LineBreakLabels(List<string> instructions)
@@ -29,13 +71,11 @@ namespace Sharp_LR35902_Assembler
 			}
 		}
 
-		public static void RemoveWhitespaceAndAddComma(List<string> instructions)
+		public static void AddComma(List<string> instructions)
 		{
 			for (var i = 0; i < instructions.Count; i++)
 			{
-				var line = instructions[i].Trim();
-				while (line.Contains("  "))
-					line = line.Replace("  ", " ");
+				var line = instructions[i];
 
 				var lastspace = line.LastIndexOf(' ');
 				if (lastspace > line.IndexOf(' '))
