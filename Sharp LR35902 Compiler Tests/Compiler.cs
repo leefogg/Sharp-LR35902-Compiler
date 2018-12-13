75b3bdcbc7655d3dc40cbcc60bfc3ebcc8be34c6
Lee Fogg <leefogg@users.noreply.github.com>
2018-12-13 13:37:49 +0000

    Added basic naive register allocation

---
+++ Sharp LR35902 Compiler Tests/Compiler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sharp_LR35902_Compiler;
+using Sharp_LR35902_Compiler.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static Sharp_LR35902_Compiler.Compiler;
+
+namespace Sharp_LR35902_Compiler_Tests
+{
+	[TestClass]
+	public class Compiler
+	{
+		[TestMethod]
+		public void BasicRegisterAlloc()
+		{
+			var expectedvariables = new[] { "x", "y", "z" };
+			var rootnode = new Node();
+			rootnode.AddChild(new VariableDeclarationNode("int", expectedvariables[0])); // int x
+			rootnode.AddChild(new VariableAssignmentNode(expectedvariables[0], new ImmediateValueNode(5))); // x = 5
+			rootnode.AddChild(new VariableDeclarationNode("int", expectedvariables[1])); // int y
+			rootnode.AddChild(new VariableAssignmentNode(expectedvariables[0], new VariableValueNode(expectedvariables[1]))); // x = y
+			rootnode.AddChild(new VariableDeclarationNode("int", expectedvariables[2])); // int y
+
+			var alloc = NaiveAllocate(rootnode);
+			Assert.AreEqual(3, alloc.Keys.Count);
+
+			var i = 0;
+			foreach (var variable in alloc.Keys)
+				Assert.AreEqual(expectedvariables[i++], variable);
+			i = 0;
+			foreach (var register in alloc.Values)
+				Assert.AreEqual(register, i++);
+		}
+	}
+}
---
+++ Sharp LR35902 Compiler/Compiler.cs
@@ -0,0 +1,30 @@
+﻿using Sharp_LR35902_Compiler.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp_LR35902_Compiler
+{
+    public class Compiler
+	{
+		public static IDictionary<string, int> AllocateRegisters(Node astroot)
+		{
+			var start = NaiveAllocate(astroot);
+			// TODO: Find last usage of each register and reuse in a loop
+			return start;
+		}
+
+		public static IDictionary<string, int> NaiveAllocate(Node astroot)
+		{
+			var variabletoregister = new Dictionary<string, int>();
+			var currentnode = 0;
+
+			foreach (var node in astroot.GetChildren()) {
+				if (node is VariableDeclarationNode dec)
+					variabletoregister[dec.Name] = currentnode++;
+			}
+
+			return variabletoregister;
+		}
+	}
+}
