25ef00ed4e15d29c1c91e2e4378af42047fd0672
Lee Fogg <leefogg@users.noreply.github.com>
2018-12-14 16:30:54 +0000

    Basic register allocation optimization

--- Sharp LR35902 Compiler Tests/Compiler.cs
+++ Sharp LR35902 Compiler Tests/Compiler.cs
@@ -3,6 +3,7 @@ using Sharp_LR35902_Compiler;
 using Sharp_LR35902_Compiler.Nodes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 using static Sharp_LR35902_Compiler.Compiler;
@@ -20,8 +21,8 @@ namespace Sharp_LR35902_Compiler_Tests
 			rootnode.AddChild(new VariableDeclarationNode("int", expectedvariables[0])); // int x
 			rootnode.AddChild(new VariableAssignmentNode(expectedvariables[0], new ImmediateValueNode(5))); // x = 5
 			rootnode.AddChild(new VariableDeclarationNode("int", expectedvariables[1])); // int y
-			rootnode.AddChild(new VariableAssignmentNode(expectedvariables[0], new VariableValueNode(expectedvariables[1]))); // x = y
-			rootnode.AddChild(new VariableDeclarationNode("int", expectedvariables[2])); // int y
+			rootnode.AddChild(new VariableAssignmentNode(expectedvariables[1], new VariableValueNode(expectedvariables[0]))); // y = x
+			rootnode.AddChild(new VariableDeclarationNode("int", expectedvariables[2])); // int z
 
 			var alloc = NaiveAllocate(rootnode);
 			Assert.AreEqual(3, alloc.Keys.Count);
@@ -32,6 +33,77 @@ namespace Sharp_LR35902_Compiler_Tests
 			i = 0;
 			foreach (var register in alloc.Values)
 				Assert.AreEqual(register, i++);
+		}
+
+		[TestMethod]
+		public void FindsLastVariableUsage()
+		{
+			var rootnode = new Node();
+			rootnode.AddChild(new VariableDeclarationNode("int", "x")); // int x
+			rootnode.AddChild(new VariableAssignmentNode("x", new ImmediateValueNode(5))); // x = 5
+			rootnode.AddChild(new VariableDeclarationNode("int", "y")); // int y
+			rootnode.AddChild(new VariableAssignmentNode("y", new VariableValueNode("x"))); // y = x
+			rootnode.AddChild(new VariableDeclarationNode("int", "z")); // int z
+
+			var lastusage = FindLastVariableUsage(rootnode, 0, "x");
+			Assert.AreEqual(3, lastusage);
+		}
+
+		[TestMethod]
+		public void FindsAllLastUses()
+		{
+			var rootnode = new Node();
+			rootnode.AddChild(new VariableDeclarationNode("int", "x")); // int x
+			rootnode.AddChild(new VariableAssignmentNode("x", new ImmediateValueNode(5))); // x = 5
+			rootnode.AddChild(new VariableDeclarationNode("int", "y")); // int y
+			rootnode.AddChild(new VariableAssignmentNode("y", new VariableValueNode("x"))); // y = x
+			rootnode.AddChild(new VariableDeclarationNode("int", "z")); // int z
+
+			var lastuses = FindAllLastUsages(rootnode).ToArray();
+			Assert.AreEqual(3, lastuses.Length, "Not all variables found");
+			Assert.AreEqual("x", lastuses[0].Name);
+			Assert.AreEqual(0, lastuses[0].Start);
+			Assert.AreEqual(3, lastuses[0].End);
+			Assert.AreEqual("y", lastuses[1].Name);
+			Assert.AreEqual(2, lastuses[1].Start);
+			Assert.AreEqual(3, lastuses[1].End);
+			Assert.AreEqual("z", lastuses[2].Name);
+			Assert.AreEqual(4, lastuses[2].Start);
+			Assert.AreEqual(4, lastuses[2].End);
+		}
+
+		[TestMethod]
+		public void ReallocatesRegisters_Simple()
+		{
+			var timelines = new[]
+			{
+				new VariableUseRage("x", 0,1), // int x = 1
+				new VariableUseRage("y", 2,3), // int y = 1
+				new VariableUseRage("z", 4,5), // int z = 1
+			};
+
+			var newallocations = OptimizeAllocation(timelines);
+			Assert.AreEqual(3, newallocations.Keys.Count);
+			Assert.AreEqual(0, newallocations["x"]);
+			Assert.AreEqual(0, newallocations["y"]);
+			Assert.AreEqual(0, newallocations["z"]);
+		}
+
+		[TestMethod]
+		public void ReallocatesRegisters_Overlap()
+		{
+			var timelines = new[]
+			{
+				new VariableUseRage("x", 0,5), 
+				new VariableUseRage("y", 2,3), 
+				new VariableUseRage("z", 4,5),
+			};
+
+			var newallocations = OptimizeAllocation(timelines);
+			Assert.AreEqual(3, newallocations.Keys.Count);
+			Assert.AreEqual(0, newallocations["x"]);
+			Assert.AreEqual(1, newallocations["y"]);
+			Assert.AreEqual(1, newallocations["z"]);
 		}
 	}
 }
--- Sharp LR35902 Compiler/Compiler.cs
+++ Sharp LR35902 Compiler/Compiler.cs
@@ -1,12 +1,26 @@
 ﻿using Sharp_LR35902_Compiler.Nodes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Sharp_LR35902_Compiler
 {
     public class Compiler
 	{
+		public struct VariableUseRage
+		{
+			public string Name;
+			public int Start, End;
+
+			public VariableUseRage(string name, int start, int end)
+			{
+				Name = name;
+				Start = start;
+				End = end;
+			}
+		}
+
 		public static IDictionary<string, int> AllocateRegisters(Node astroot)
 		{
 			var start = NaiveAllocate(astroot);
@@ -25,6 +39,73 @@ namespace Sharp_LR35902_Compiler
 			}
 
 			return variabletoregister;
+		}
+
+		public static IDictionary<string, int> OptimizeAllocation(IEnumerable<VariableUseRage> variablelifetimes)
+		{
+			var allocations = new Dictionary<string, int>();
+			var unallocatedvariables = variablelifetimes.ToList();
+
+			var registerlifetimes = new int[unallocatedvariables.Count];
+
+			var currentregister = 0;
+			while (unallocatedvariables.Any())
+			{
+				for (var i=0; i<unallocatedvariables.Count; i++)
+				{
+					var variable = unallocatedvariables[i];
+					if (registerlifetimes[currentregister] <= variable.Start)
+					{
+						allocations[variable.Name] = currentregister;
+						registerlifetimes[currentregister] = variable.End;
+						unallocatedvariables.RemoveAt(i);
+						i--;
+					}
+					// Register start positions always increase. Will never find a variable assigned before registerlifetime.Start
+				}
+
+				currentregister++;
+			}
+
+			return allocations;
+		}
+
+		public static IEnumerable<VariableUseRage> FindAllLastUsages(Node rootnode)
+		{
+			var lastuses = new Dictionary<string, VariableUseRage>();
+
+			var index = 0;
+			foreach (var node in rootnode.GetChildren())
+			{
+				if (node is VariableDeclarationNode dec)
+				{
+					var lastusage = FindLastVariableUsage(rootnode, index, dec.Name);
+					yield return new VariableUseRage(dec.Name, index, lastusage);
+				}
+
+				index++;
+			}
+		}
+
+		public static int FindLastVariableUsage(Node rootnote, int start, string variablename)
+		{
+			int lastuseage = start;
+
+			var children = rootnote.GetChildren();
+			for (var i=start+1; i<children.Length; i++)
+			{
+				var node = children[i];
+				if (node is VariableAssignmentNode assignment)
+				{
+					if (assignment.VariableName == variablename)
+						lastuseage = i;
+					if (assignment.Value is VariableValueNode val)
+						if (val.Name == variablename)
+							lastuseage = i;
+				}
+			}
+
+			return lastuseage;
 		}
 	}
 }
